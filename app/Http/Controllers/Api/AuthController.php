1cf9069fb9728631c0007b27dd550650843e5bae
ariqfrhan <muhammadariq3@gmail.com>
2024-02-10 23:21:28 +0700

    `feat`: register with upload image and wallet

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,11 +7,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 use App\Models\Wallet;
+use Melihovv\Base64ImageDecoder\Base64ImageDecoder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         $data = $request->all();
 
         $validator = Validator::make($data, [
@@ -25,9 +30,70 @@ class AuthController extends Controller
             return response()->json(['errors' => $validator->messages()], 400);
         }
 
-        $user = User::where('email', $request->email)->exist();
+        $user = User::where('email', $request->email)->exists();
         if ($user) {
-            return response()->json(['message'=>'Email already exist!'], 409);
+            return response()->json(['message' => 'Email already exist!'], 409);
         }
+
+        DB::beginTransaction();
+        try {
+            $profilePicture = null;
+            $ktp = null;
+
+            if ($request->profile_picture) {
+                $profilePicture = $this->uploadBase64Image($request->profile_picture);
+            }
+            if ($request->ktp) {
+                $ktp = $this->uploadBase64Image($request->ktp);
+            }
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'username' => $request->name,
+                'password' => bcrypt($request->password),
+                'profile_picture' => $profilePicture,
+                'ktp' => $ktp,
+                'verified' => ($ktp) ? true : false
+            ]);
+
+            Wallet::create([
+                'user_id' => $user->id,
+                'balance' => 0,
+                'pin' => $request->pin,
+                'card_number' =>$this->generateCardNumber(16)
+            ]);
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return response()->json(['message' => $th->getMessage()], 500);
+        }
+    }
+
+    private function generateCardNumber($length){
+        $result = '';
+        for ($i=0; $i < $length ; $i++) { 
+            $result .= mt_rand(0,9);
+        }
+
+        $wallet = Wallet::where('card_number', $result)->exists();
+        if ($wallet) {
+            return $this-> generateCardNumber($length);
+        }
+        return $result;
+    }
+
+    private function uploadBase64Image($base64image)
+    {
+        $decoder = new Base64ImageDecoder($base64image, $allowedFormats = ['jpeg', 'png', 'gif']);
+
+        $decodedContent = $decoder->getDecodedContent();
+        $format = $decoder->getFormat();
+        $image = Str::random(10) . '.' . $format;
+
+        Storage::disk('public')->put($image, $decodedContent);
+
+        return $image;
     }
 }
