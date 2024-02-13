ff0e4e1e4e4f942ec2686de22e40490d39dbe9ee
ariqfrhan <muhammadariq3@gmail.com>
2024-02-13 18:02:43 +0700

    `feat` : admin page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\TransactionController;
 use App\Http\Controllers\RedirectPaymentController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +17,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 
 Route::get('payment_finish', [RedirectPaymentController::class, 'finish']);
+
+Route::group(['prefix'=>'admin'], function(){
+    Route::view('/login', 'login')->name('admin.auth.index');
+    Route::get('logout', [AuthController::class, 'logout'])->name('admin.auth.logout');
+    Route::post('login', [AuthController::class, 'login'])->name('admin.auth.login');
+
+    Route::group(['middleware'=>'auth:web'], function(){
+        Route::view('/', 'dashboard')->name('admin.dashboard');
+        Route::get('transactions', [TransactionController::class, 'index'])->name('admin.transaction.index');
+    });
+
+});
